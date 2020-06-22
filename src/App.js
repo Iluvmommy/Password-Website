58cd227f94df10fb1a60fa5efe3bb872f2ef40f1
Curtis <curtis.hu688@gmail.com>
2020-06-22 10:13:24 -0700

    routing officially finished

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PasswordChecker from "./components/password-checker";
 import "./App.css";
 import { BrowserRouter as router, Route, Switch } from "react-router-dom";
 import PasswordGenerator from "./components/password-generate";
+import Home from "./components/home";
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <NavBar />
       <router>
         <Switch>
-          <Route path="/" exact component={PasswordChecker}/>
+          <Route path="/" exact component={Home}/>
+          <Route path="/password-checker" exact component={PasswordChecker}/>
           <Route path="/password-generator" component={PasswordGenerator}/>
         </Switch>
       </router>
