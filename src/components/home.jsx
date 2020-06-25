386960968337d05b5021a8ec957d89de39836384
Curtis <curtis.hu688@gmail.com>
2020-06-24 21:17:02 -0700

    background setup

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,15 +1,21 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./css/general.css";
+import "./css/home.css";
 
 class Home extends Component {
-    state = {  }
-    render() { 
-        return ( 
-            <>
-            <h1>Password Safety</h1>
-            <p></p>
-            </>
-         );
-    }
+  state = {};
+  render() {
+    return (
+      <div id="main">
+        <div className="text">
+          <h1 className="font">Password Safety</h1>
+          <p className="font">
+            Hacker attack every <em>39</em> seconds
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
- 
+
 export default Home;
