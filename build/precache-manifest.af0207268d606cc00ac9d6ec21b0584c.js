b0ebeacfd002c9081bba9f3b57514c208f34aa7e
Curtis <curtis.hu688@gmail.com>
2020-06-26 16:35:14 -0700

    build created

---
+++ build/precache-manifest.af0207268d606cc00ac9d6ec21b0584c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b5d6bd6d64790c5093a9f91a120bbb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0f746fffb4c717f577e",
+    "url": "/static/css/main.daf82445.chunk.css"
+  },
+  {
+    "revision": "61addd3f96edf54f4651",
+    "url": "/static/js/2.868b92c3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.868b92c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f746fffb4c717f577e",
+    "url": "/static/js/main.d878702b.chunk.js"
+  },
+  {
+    "revision": "af99edab7c3a110db6a6",
+    "url": "/static/js/runtime-main.4bbf50f9.js"
+  }
+]);
